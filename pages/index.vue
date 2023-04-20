d55525b41c2c614e8af7c73d1664ecea0efbab29
MeepsKitten <meepsalot69@gmail.com>
2023-04-20 11:15:10 -0700

    new home

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to the Home Page</h1>
-<p>This is some placeholder text for the home page.</p>
-<nav>
-<nuxt-link to="/">Home</nuxt-link>
-<nuxt-link to="/perks">Perks</nuxt-link>
-
-</nav>
+    <iframe src="https://store.steampowered.com/widget/774801/?t=Claw%20your%20way%20across%20exotic%20islands%20combining%20fluid%20movement%20with%20fast%20paced%20combat%20to%20become%20a%20Crab%20Champion%20in%20this%20third%20person%20shooter%20with%20roguelike%20elements." frameborder="0" width="100%" height="290"></iframe>
 
   </div>
 </template>
