b1749ada3c0188424a14e8ba000e4d5c17b67c54
MeepsKitten <meepsalot69@gmail.com>
2023-04-20 06:17:07 -0700

    removed placeholders

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
-    <h1>Welcome to the Home Page</h1]string>
+    <h1>Welcome to the Home Page</h1>
 <p>This is some placeholder text for the home page.</p>
 <nav>
 <nuxt-link to="/">Home</nuxt-link>
 <nuxt-link to="/perks">Perks</nuxt-link>
-<!-- Placeholder tabs -->
-<NuxtLink to="/placeholder1">Placeholder 1</NuxtLink>
-<NuxtLink to="/placeholder2">Placeholder 2</NuxtLink>
-<NuxtLink to="/placeholder3">Placeholder 3</NuxtLink>
+
 </nav>
 
   </div>
